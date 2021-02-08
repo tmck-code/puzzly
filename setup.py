18d21397e7b83eb8d9e2686caacf087da9c77749
Tom McKeesick <tmck01@gmail.com>
2021-02-08 22:00:45 +1100

    Add boilerplate pip package definition

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'The Punsy pip package'
+
+import setuptools
+
+def readme():
+    'Return README.md as a string'
+    with open('README.md', 'r') as istream:
+        return istream.read()
+
+setuptools.setup(
+    name='puzzly',
+    version='0.0.1',
+    author='Tom McKeesick',
+    author_email='tmck01@gmail.com',
+    description='A puzzle solver & generator',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tmck-code/puzzly',
+    packages=setuptools.find_packages(),
+    package_data={
+        'puzzly': []
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'puzzly = puzzly.run:main',
+        ],
+    },
+    install_requires=[]
+)
